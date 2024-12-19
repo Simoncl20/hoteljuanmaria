d5d869178cd342cd1a5c45e9cb655475960bd869
Juan <jj.diazo1@uniandes.edu.co>
2024-12-19 17:54:26 -0500

    footer tailwind y comentado

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,54 +1,56 @@
 
-import styles from './footer.module.css';
-
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.container}>
-        <div className={styles.logo}>
-          <h2 className={styles.title}>Hotel Juan María</h2>
-          <p className={styles.subtitle}>Mejorando Momentos</p>
-          <p className={styles.description}>
+    <footer className="bg-[#1b1816] text-white py-8 px-4 font-sans">
+      <div className="flex flex-wrap justify-between">
+        {/* Logo y descripción */}
+        <div className="flex-1 mr-8">
+          <h2 className="text-2xl font-bold mb-2">Hotel Juan María</h2>
+          <p className="text-lg mb-4">Mejorando Momentos</p>
+          <p className="text-sm leading-relaxed mb-6">
             Asegurando tu felicidad: la satisfacción de nuestros huéspedes es lo primero, superando siempre las expectativas.
           </p>
-          <div className={styles.social}>
-            <i className="fa-brands fa-facebook"></i>
-            <i className="fa-brands fa-twitter"></i>
-            <i className="fa-brands fa-youtube"></i>
-            <i className="fa-solid fa-phone"></i>
+          <div className="flex space-x-4 text-xl">
+            <i className="fa-brands fa-facebook cursor-pointer"></i>
+            <i className="fa-brands fa-twitter cursor-pointer"></i>
+            <i className="fa-brands fa-youtube cursor-pointer"></i>
+            <i className="fa-solid fa-phone cursor-pointer"></i>
           </div>
         </div>
 
-        <div className={styles.linksSection}>
-          <div className={styles.column}>
-            <h3>Empresa</h3>
-            <ul>
-              <li>Inicio</li>
-              <li>Sobre Nosotros</li>
-              <li>Habitaciones</li>
-              <li>Servicios</li>
-              <li>Paquetes</li>
+        {/* Enlaces de la empresa */}
+        <div className="flex flex-2 justify-between space-x-8">
+          <div>
+            <h3 className="text-lg mb-4">Empresa</h3>
+            <ul className="list-none m-0 p-0">
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Inicio</li>
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Sobre Nosotros</li>
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Habitaciones</li>
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Servicios</li>
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Paquetes</li>
             </ul>
           </div>
 
-          <div className={styles.column}>
-            <h3>Enlaces Útiles</h3>
-            <ul>
-              <li>Blog</li>
-              <li>Personal</li>
-              <li>Preguntas Frecuentes</li>
-              <li>Testimonios</li>
-              <li>Reservas</li>
+          {/* Enlaces útiles */}
+          <div>
+            <h3 className="text-lg mb-4">Enlaces Útiles</h3>
+            <ul className="list-none m-0 p-0">
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Blog</li>
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Personal</li>
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Preguntas Frecuentes</li>
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Testimonios</li>
+              <li className="text-sm mb-2 cursor-pointer hover:underline">Reservas</li>
             </ul>
           </div>
 
-          <div className={styles.column}>
-            <h3>Contacto</h3>
-            <ul>
-              <li>info@hotelresort.com</li>
-              <li>+32466699900</li>
-              <li>Jl Nuansa Anyelir, Ubud</li>
-              <li>Memory, NY 9066</li>
+          {/* Información de contacto */}
+          <div>
+            <h3 className="text-lg mb-4">Contacto</h3>
+            <ul className="list-none m-0 p-0">
+              <li className="text-sm mb-2">info@hotelresort.com</li>
+              <li className="text-sm mb-2">+32466699900</li>
+              <li className="text-sm mb-2">Jl Nuansa Anyelir, Ubud</li>
+              <li className="text-sm mb-2">Memory, NY 9066</li>
             </ul>
           </div>
         </div>
